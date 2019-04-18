ee86dc91b30f8e4c68c0c1c4562eb3b3cfdc2458
Matix97 <46028745+Matix97@users.noreply.github.com>
2019-04-18 14:53:38 +0200

    Merge pull request #6 from IIS-ZPI/difference
    interfejs odejmowania

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
-public class Main {
+public class Main implements IArithmeticsDiff{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         System.out.println("EEIAPany Matixz Developer");
+    }
+
+    @Override
+    public double difference(double A, double B) {
+        return A-B;
     }
 }
