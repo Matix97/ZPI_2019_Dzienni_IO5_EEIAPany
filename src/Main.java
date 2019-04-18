0b25d3e712b1b8ffa3020162a3899afe89066140
Leherguargin <209376@edu.p.lodz.pl>
2019-04-18 13:34:38 +0200

    dodanie wlasnej nazwy oraz roli w zespole

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("EEIAPany Matixz Developer");
+        System.out.println("Leherguargin Operations");
     }
 }
