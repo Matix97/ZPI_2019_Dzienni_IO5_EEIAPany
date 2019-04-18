c85efdc255f0b57e3af4570555556a756ad97450
Matixz <43850200+Matixz@users.noreply.github.com>
2019-04-18 14:37:57 +0200

    Merge pull request #2 from IIS-ZPI/WivorDisplayingID
    zad 2 - dodanie wasnego id
    
    Cherry-pick

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main implements IArithmeticsDiff{
 
     public static void main(String[] args)  {
         System.out.println("EEIAPany Matixz Developer");
+        System.out.println("EEIAPany Wivor Developer");
+        System.out.println("Matixz Developer");
     }
 
     @Override
