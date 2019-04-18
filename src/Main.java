0d0248e2e328fb7eed60e8f995edc473b5ec5ca3
Leherguargin <209376@edu.p.lodz.pl>
2019-04-18 14:07:36 +0200

    dodanie interfejsu implementujacego mnozenie dwoch liczb double

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Main implements IAritmeticsMult{
 
     public static void main(String[] args) {
         System.out.println("EEIAPany Matixz Developer");
@@ -6,5 +6,10 @@ public class Main {
         System.out.println("Matix97 Tester");
         System.out.println("Wivor Developer");
         System.out.println("Matixz Developer");
+    }
+
+    @Override
+    public double multiplication(double a, double b) {
+        return a*b;
     }
 }
