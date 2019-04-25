68cbe2b19883125f6f3d3290ac4beed103571b7a
Leherguargin <maciejleszczynski1997@gmail.com>
2019-04-25 14:07:16 +0200

    kom2

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main implements IArithmeticsDiff, IArithmeticsAdd, IArithmeticDiv, 
     public double division(double A, double B) {
         return A/B;
     }
-
+    //cokolwiek gdzieindziej 1
     @Override
     public double addition(double A, double B) {
         return A+B;
